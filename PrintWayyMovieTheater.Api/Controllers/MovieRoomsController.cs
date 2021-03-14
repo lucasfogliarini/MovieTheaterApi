a9bb6ccec5d0770337c2b0afa10960cbb26edb30
Lucas Fogliarini <lucasfogliarini@gmail.com>
2021-03-14 15:43:42 -0300

    MovieRoom HttpGet, create

---
+++ PrintWayyMovieTheater.Api/Controllers/MovieRoomsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PrintWayyMovieTheater.Domain.Entities;
+using PrintWayyMovieTheater.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintWayyMovieTheater.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieRoomsController : ControllerBase
+    {
+        private readonly IMovieRoomService _movieRoomService;
+
+        public MovieRoomsController(IMovieRoomService movieRoomService)
+        {
+            _movieRoomService = movieRoomService;
+        }
+
+        [HttpGet]
+        public IEnumerable<MovieRoom> Get()
+        {
+            return _movieRoomService.Get();
+        }
+    }
+}
--- PrintWayyMovieTheater.Api/Startup.cs
+++ PrintWayyMovieTheater.Api/Startup.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PrintWayyMovieTheater.Domain;
+using PrintWayyMovieTheater.Domain.Entities;
+using PrintWayyMovieTheater.Domain.Services;
 
 namespace PrintWayyMovieTheater.Api
 {
@@ -25,8 +22,8 @@ namespace PrintWayyMovieTheater.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddAllServices();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PrintWayyMovieTheater.Api", Version = "v1" });
@@ -34,7 +31,7 @@ namespace PrintWayyMovieTheater.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMovieRoomService movieRoomService)
         {
             if (env.IsDevelopment())
             {
@@ -51,6 +48,20 @@ namespace PrintWayyMovieTheater.Api
             {
                 endpoints.MapControllers();
             });
+
+            SeedMovieRooms(movieRoomService);
+        }
+
+        private void SeedMovieRooms(IMovieRoomService movieRoomService, int count = 5)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                movieRoomService.Create(new MovieRoom()
+                {
+                    Name = "Room " + i,
+                    Seats = 50 + i
+                });
+            }
         }
     }
 }
